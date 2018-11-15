cf55692e778f81109690c19faf03d29dd926896a
Jonathan Lesuperb <lesuperbjonathan@gmail.com>
2018-11-15 05:20:38 +0100

    Init

---
+++ Training1/Pages/Messaging.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Training1.Pages
+{
+    public class MessagingModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
